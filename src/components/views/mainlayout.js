bae49949cea6176fdffdb6e2f5930259801a5ea5
Eduardo Moura <morphex.e@gmail.com>
2016-09-04 16:22:25 +0100

    Fixed Dropdown not showing Fixed Error on import statment.

--- src/components/views/mainlayout.js
+++ src/components/views/mainlayout.js
@@ -2,33 +2,35 @@ import React, {Component} from 'react';
 import QueryWindow from '../SearchWindow/querybuilder';
 import MapWindow from '../MapWindow/mapView';
 import TableWindow from '../TableWindow/tableView';
-import {ButtonToolbar,MenuItem,Glyphicon} from 'react-bootstrap';
-import Dropdown, {Menu} from 'react-bootstrap';
+import {ButtonToolbar, Dropdown, Glyphicon,MenuItem} from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 
- /* eslint-disable react/no-multi-comp*/
- /* eslint-disable react/prop-types*/
+const DropdownMenu = Dropdown.Menu;
+
+
+/* eslint-disable react/no-multi-comp*/
+/* eslint-disable react/prop-types*/
 const GoldenLayout = require('golden-layout');
-   
+
 
 class CustomToggle extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-  handleClick(e) {
-    e.preventDefault();
+    handleClick(e) {
+        e.preventDefault();
 
-    this.props.onClick(e);
-  }
+        this.props.onClick(e);
+    }
 
-  render() {
-    return (
-           <Glyphicon glyph="cog" onClick={this.handleClick} />
-    );
-  }
+    render() {
+        return (
+            <Glyphicon glyph="cog" onClick={this.handleClick} />
+        );
+    }
 }
 
 
@@ -106,7 +108,7 @@ const layoutConfig = {
             }]
     }]
 };
-   // eslint-disable-next-line react/no-multi-comp
+// eslint-disable-next-line react/no-multi-comp
 
 export default class MainLayout extends Component {
 
@@ -122,7 +124,7 @@ export default class MainLayout extends Component {
             }
         });
         const alertWall = React.createClass({
-            componentDidMount:  function()  {
+            componentDidMount: function () {
 
                 const color = this.props.color || '#000000';
                 this.props.glContainer.tab.titleElement.prevObject.css('background-color', this.props.color);
@@ -132,7 +134,7 @@ export default class MainLayout extends Component {
                     tab.titleElement.prevObject.css('background-color', color);
                 });
             },
-            render: function() {
+            render: function () {
 
                 return (<h1>test component 3</h1>)
             }
@@ -154,35 +156,37 @@ export default class MainLayout extends Component {
              */
             stack.on('activeContentItemChanged', function (contentItem) {
                 stack.header.controlsContainer.children('.alertCommands').remove();
-              const buttonInstance = (
-                <ButtonToolbar>
-                    <Dropdown id="dropdown-custom-1"  >
-                    <CustomToggle bsRole="toggle">
-                        <Glyphicon glyph="cog" />
-                    </CustomToggle>
-                    <Menu className="">
-                      <MenuItem divider />
-                        <MenuItem eventKey="1">Low Priority (1)</MenuItem>
-                         <MenuItem eventKey="2" active >Medium Priority (2)</MenuItem>
-                          <MenuItem eventKey="3">High Priority (3)</MenuItem>
-                           <MenuItem eventKey="4">Highest Priority (4)</MenuItem>
-                          <MenuItem divider />
-                        <MenuItem eventKey="6">Color (Red)</MenuItem>
+                const buttonInstance = (
+                    <ButtonToolbar>
+                        <Dropdown id="dropdown-custom-1"  >
+                            <CustomToggle bsRole="toggle">
+                                <Glyphicon glyph="cog" />
+                            </CustomToggle>
+                            <DropdownMenu className="">
+                                <MenuItem divider />
+                                <MenuItem eventKey="1">Low Priority (1) </MenuItem>
+                                <MenuItem eventKey="2" active >Medium Priority (2) </MenuItem>
+                                <MenuItem eventKey="3">High Priority (3) </MenuItem>
+                                <MenuItem eventKey="4">Highest Priority (4) </MenuItem>
+                                <MenuItem divider />
+                                <MenuItem eventKey="6">Color (Red) </MenuItem>
+                                <MenuItem divider />
+                                <MenuItem eventKey="7" >Export to PNG</MenuItem>
+                                <MenuItem eventKey="8" >Export to CSV</MenuItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                    </ButtonToolbar>
 
-                        <MenuItem divider />
-                        <MenuItem eventKey="7" >Export to PNG</MenuItem>
-                        <MenuItem eventKey="8" >Export to CSV</MenuItem>
-                    </Menu>
-                    </Dropdown>
-                </ButtonToolbar>
-);
+                );
+
+
 
                 // interact with the contentItem
                 if (contentItem.config.component == 'Alert Wall') {
 
                     stack.header.controlsContainer.prepend('<li class="alertCommands "><span class="glyphicon glyphicon-volume-off" aria-hidden="true"></span></li>');
                     stack.header.controlsContainer.prepend('<li class="alertCommands cogSettings"></li>');
-                    ReactDOM.render(buttonInstance,  stack.header.controlsContainer.children('.cogSettings')[0]);
+                    ReactDOM.render(buttonInstance, stack.header.controlsContainer.children('.cogSettings')[0]);
                 }
             });
 
