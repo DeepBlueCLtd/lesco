7a5f7517f6f60ee965aae21b87eb44f42e4876fc
IanMayo <ian@planetmayo.com>
2016-09-04 11:21:40 +0100

    replace data

--- src/components/TableWindow/tableView.js
+++ src/components/TableWindow/tableView.js
@@ -2,16 +2,19 @@ import React, {Component} from 'react'
 import SlickGrid from 'react-slickgrid';
 
 const tableData = [
-    { name: 'Johnny Appleseed', profession: 'Engineer', age: 31 },
-    { name: 'Johnny ', profession: 'Engineer', age: 31 },
-    { name: 'Appleseed', profession: 'Engineer', age: 31 },
-    { name: ' adasd ', profession: 'Engineer', age: 31 },
-    { name: 'Jonas', profession: 'Engineer', age: 31 },
-    { name: 'Peter', profession: 'Engineer', age: 31 },
-    { name: 'Own', profession: 'Engineer', age: 31 },
-    { name: 'Eduardo ', profession: 'Engineer', age: 31 },
-    { name: 'Ian', profession: 'Engineer', age: 31 },
-    { name: 'Osama', profession: 'Bad Guy', age: 64 }
+    { Time: '12/09/17 16:34', User: 'userA', Location: '12.3N 1.3W', Source: 'Twitter', Message: 'message a', Keywords:'keyword-a' },
+    { Time: '12/09/17 16:28', User: 'userC', Location: '12.4N 1.5W', Source: 'Twitter', Message: 'message b', Keywords:'keyword-b' },
+    { Time: '12/09/17 16:25', User: 'userA', Location: '11.1N 2.1W', Source: 'Facebook', Message: 'message c', Keywords:'keyword-c' },
+    { Time: '12/09/17 16:21', User: 'userB', Location: '11.7W 1.3W', Source: 'Instagram', Message: 'message d', Keywords:'keyword-a' },
+    { Time: '12/09/17 16:20', User: 'userA', Location: '13.9N 1.2W', Source: 'Facebook', Message: 'message e', Keywords:'keyword-a' },
+    { Time: '12/09/17 16:19', User: 'userD', Location: '13.4N 2.8W', Source: 'Instragram', Message: 'message f', Keywords:'keyword-c' },
+    { Time: '12/09/17 16:18', User: 'userC', Location: '12.4N 1.5W', Source: 'Twitter', Message: 'message b', Keywords:'keyword-b' },
+    { Time: '12/09/17 16:15', User: 'userA', Location: '11.1N 2.1W', Source: 'Facebook', Message: 'message c', Keywords:'keyword-c' },
+    { Time: '12/09/17 16:11', User: 'userB', Location: '11.7W 1.3W', Source: 'Instagram', Message: 'message d', Keywords:'keyword-a' },
+    { Time: '12/09/17 16:07', User: 'userD', Location: '11.1N 1.3W', Source: 'Facebook', Message: 'message f', Keywords:'keyword-f' },
+    { Time: '12/09/17 16:05', User: 'userA', Location: '11.1N 2.1W', Source: 'Facebook', Message: 'message c', Keywords:'keyword-c' },
+    { Time: '12/09/17 16:01', User: 'userB', Location: '11.7W 1.3W', Source: 'Instagram', Message: 'message d', Keywords:'keyword-a' },
+    { Time: '12/09/17 16:00', User: 'userA', Location: '13.9N 1.2W', Source: 'Facebook', Message: 'message e', Keywords:'keyword-a' },
 ]
 
 export default class TableView extends Component {
